b755f4b625487a0064551b2d6c73e523db606312
Daniel García Jones <daniel.garcia@es.unisys.com>
2016-11-04 14:22:21 +0100

    Added handling One and Many functions

--- auxiliar.js
+++ auxiliar.js
@@ -121,5 +121,40 @@ module.exports = {
         } else {
             return false;
         }
+    },
+    
+    handleMany: function(property, res, error, result) {
+
+        if (error) {
+            console.log(error);
+            return res.
+                status(status.INTERNAL_SERVER_ERROR).
+                json({ error: error.toString() });
+        }
+
+        var json = {};
+        json[property] = result;
+        res.json(json);
+
+    },
+
+    handleOne: function(property, res, error, result) {
+    
+        if (error) {
+            return res.
+                    status(status.INTERNAL_SERVER_ERROR).
+                    json({ error: error.toString() });
+        }
+        if (!result) {
+            return res.
+                status(status.NOT_FOUND).
+                json({ error: 'Not found' });
+        }
+
+        var json = {};
+        json[property] = result;
+        res.json(json);
+    
     }
+    
 }
